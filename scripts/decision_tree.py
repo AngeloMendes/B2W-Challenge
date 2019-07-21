6551cc32c64e201e26b8910c17a608de34bf97d3
Angelo Mendes <contato.angelomendes@gmail.com>
2019-07-20 22:40:20 -0300

    add tree to all products

--- scripts/decision_tree.py
+++ scripts/decision_tree.py
@@ -7,11 +7,11 @@ from IPython.display import Image
 df_sales = pd.read_csv('../datasets/sales.csv', delimiter=',')
 df_comp_prices = pd.read_csv('../datasets/comp_prices.csv', delimiter=',')
 
-
-clf = tree.DecisionTreeClassifier(max_leaf_nodes=10)
+clf = tree.DecisionTreeClassifier(max_leaf_nodes=15)
 df_comp_prices['DATE_EXTRACTION'] = pd.to_datetime(df_comp_prices['DATE_EXTRACTION']).astype(int) / 10 ** 9
 df_comp_prices['PROD_ID'] = df_comp_prices['PROD_ID'].astype('category').cat.codes
-df_comp_prices=df_comp_prices[df_comp_prices.PROD_ID==1]
+
+df_comp_prices = df_comp_prices[df_comp_prices.PROD_ID == 5]
 
 class_names = df_comp_prices.COMPETITOR.unique()
 feature_names = ['PROD_ID', 'DATE_EXTRACTION', 'COMPETITOR_PRICE', 'PAY_TYPE']
@@ -21,9 +21,8 @@ df_comp_prices['COMPETITOR'] = df_comp_prices['COMPETITOR'].astype('category').c
 clf = clf.fit(df_comp_prices[['PROD_ID', 'DATE_EXTRACTION', 'COMPETITOR_PRICE', 'PAY_TYPE']],
               df_comp_prices['COMPETITOR'])
 
-
 dot_data = tree.export_graphviz(clf, out_file=None, feature_names=feature_names, class_names=class_names,
                                 filled=True, rounded=True, special_characters=True)
 graph = pydotplus.graph_from_dot_data(dot_data)
 Image(graph.create_png())
-graph.write_png("../analysis/decision_tree.png")
+graph.write_png("../analysis/decision_tree_prod.png")
